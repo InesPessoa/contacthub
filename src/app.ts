da753abeade8c89b33ea942e20872f215b94c514
Ines Pessoa <inesmspessoa@gmail.com>
2023-08-11 00:48:19 +0100

    database configuration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express, { Application, Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// Environment variables
+dotenv.config({ path: ".env" });
+
+// DB connection
+mongoose
+  .connect(
+    `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/contacthub`
+  )
+  .then((): void => console.log("Database connected"))
+  .catch((err: Error): void => console.log(err));
+
+// App
 const app: Application = express();
 
-const PORT: number = 3001;
+const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 app.use("/", (req: Request, res: Response): void => {
   res.send("Hello world!");
