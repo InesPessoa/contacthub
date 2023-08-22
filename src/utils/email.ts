3166d744d77b66773e6a89b6dcf864a8c6de1810
Ines Pessoa <inesmspessoa@gmail.com>
2023-08-22 23:10:21 +0100

    refactoring of email file

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -2,27 +2,20 @@ import { createTransport } from 'nodemailer';
 import { Options } from 'nodemailer/lib/smtp-transport';
 import process from 'process';
 
-interface MailOptions {
-  emailFrom: string;
-  emailTo: string;
-  subject: string;
-  message: string;
-}
-
 const transporter = createTransport({
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
+  port: process.env.EMAIL_PORT ? parseInt(process.env.EMAIL_PORT) : 1025, //add authentication
 } as Options);
 
-export const sendEmail = async (options: MailOptions): Promise<void> => {
+export const sendEmail = async (
+  emailFrom: string,
+  emailTo: string,
+  subject: string,
+  message: string
+): Promise<void> => {
   await transporter.sendMail({
-    from: options.emailFrom,
-    to: options.emailTo,
-    subject: options.subject,
-    text: options.message,
+    from: emailFrom,
+    to: emailTo,
+    subject: subject,
+    text: message,
   });
 };
